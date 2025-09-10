325d2178ac28ae7d01d45b206a256247ee858d4d
SalemElbermawy <salemsalemahmed761@gmail.com>
2025-09-10 03:20:59 +0300

    fix: remove node-fetch and use built-in fetch

--- netlify/functions/getWeather.js
+++ netlify/functions/getWeather.js
@@ -1,11 +1,12 @@
-const fetch = require('node-fetch'); // احذف ده
-
 exports.handler = async function(event, context) {
   try {
     const city = event.queryStringParameters.city;
-    const apiKey = "c277de7b55594a17731f1fc905f115ba";
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
-    
+    const apiKey = "c277de7b55594a17731f1fc905f115ba"; // ضع مفتاح OpenWeather هنا
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    );
+
     if (!response.ok) {
       return {
         statusCode: response.status,
@@ -14,6 +15,7 @@ exports.handler = async function(event, context) {
     }
 
     const data = await response.json();
+
     return {
       statusCode: 200,
       body: JSON.stringify(data),
